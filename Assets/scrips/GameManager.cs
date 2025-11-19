6bbf81be49b7b2dd07e7724bbd193342c041186d
Ose <jobuma05@gmail.com>
2025-11-19 17:41:10 +0100

    tercer commit

---
+++ Assets/scrips/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text textoPuntos;
+    private int puntos = 0;
+    private int vida;
+    public TMP_Text textoVidaValor;
+    public Jugador jugador;       
+    public GameObject puntoInicial;  
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        puntos = 0;
+        textoPuntos.text = "0";
+
+        vida = 2;
+        textoVidaValor.text = vida.ToString();
+    }
+
+    public void SumarPuntos()
+    {
+        puntos++;
+        textoPuntos.text = puntos.ToString();
+    }
+
+     public void QuitarVida()
+    {
+        vida--;
+        textoVidaValor.text = vida.ToString();
+    }
+
+}
